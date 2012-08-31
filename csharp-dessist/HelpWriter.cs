23b914f3677a00c00f3d2ee2b0a46fc0463efb50
glassware1975 <glassware1975@a14cc1e1-e7ef-6064-31bc-8011e45929e1>
2012-08-31 17:55:50 +0000

    Improvements to the help system for clarity Wrote all import errors to the import errors file Encouraged users to share samples.

---
+++ csharp-dessist/HelpWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_dessist
+{
+    public class HelpWriter
+    {
+        public static List<string> _help_messages = new List<string>();
+
+        public static void Help(SsisObject obj, string message)
+        {
+            string s = null;
+            if (obj == null) {
+                s = "Help! " + message;
+            } else {
+                s = String.Format("Help!  Problem in {0}: {1}", obj.GetNearestGuid(), message);
+            }
+            _help_messages.Add(s);
+            //Console.WriteLine(s);
+        }
+    }
+}
