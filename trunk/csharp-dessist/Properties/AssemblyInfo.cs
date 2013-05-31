c4bae68f3a8932c5c6b63b6c425db2c7dcd70a23
glassware1975 <glassware1975@a14cc1e1-e7ef-6064-31bc-8011e45929e1>
2013-05-31 17:01:52 +0000

    Updated project to include application path Updated version number to 1.9

--- trunk/csharp-dessist/Properties/AssemblyInfo.cs
+++ trunk/csharp-dessist/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("csharp-dessist")]
-[assembly: AssemblyCopyright("Copyright (C) Ted Spence 2012")]
+[assembly: AssemblyCopyright("Copyright (C) Ted Spence 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
